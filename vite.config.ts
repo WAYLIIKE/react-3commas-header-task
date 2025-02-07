2fd54730995d854e0887546d110aae2e13a4cb4a
WAYLIIKE <valentynsavitskyy@gmail.com>
2025-02-07 16:33:05 +0100

    update config for gh-pages refreshing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { copyFileSync } from 'fs';
 
 export default defineConfig({
-  plugins: [react()],
   base: '/react-3commas-header-task/',
+  plugins: [
+    react(),
+    {
+      name: 'create-404-page',
+      apply: 'build',
+      writeBundle() {
+        copyFileSync('dist/index.html', 'dist/404.html');
+      },
+    },
+  ],
+  build: {
+    outDir: 'dist',
+  },
 });

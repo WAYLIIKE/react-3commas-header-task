d8d2464d479bf3fe2bd937ce002c1ecac602908f
WAYLIIKE <valentynsavitskyy@gmail.com>
2025-02-07 18:00:43 +0100

    update routing for gh-pages refreshing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
@@ -6,7 +6,7 @@ const ErrorPage = lazy(() => import('./pages/ErrorPage/ErrorPage'));
 
 export const App = () => {
   return (
-    <Router basename="/react-3commas-header-task">
+    <Router>
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="*" element={<ErrorPage />} />

8ef8b6bcad8786e8f854f4038cc83658d28f80b6
WAYLIIKE <valentynsavitskyy@gmail.com>
2025-02-07 17:50:18 +0100

    Updates

--- assets/HomePage-Dtu61Ie0.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BYxwSGlb.js";import{C as e,H as s}from"./Header-bNuC0Hhc.js";const o=()=>r.jsx(r.Fragment,{children:r.jsx(e,{children:r.jsx(s,{})})});export{o as default};
